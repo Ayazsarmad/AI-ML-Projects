224e54eb2e23ee2bf73c7108ce0cdbcdf5a2676f
Ayazsarmad <m.ayazsarmad@gmail.com>
2024-09-16 02:01:41 +0500

    add functions

--- Python/pythonSet2.py
+++ Python/pythonSet2.py
@@ -11,8 +11,44 @@
 
 # ------------python while loop -----------
 
-no = 1
-while no < 6:
-    print(no)
-    no = no +1 
+# no = 1
+# while no < 6:
+#     print(no)
+#     no = no +1 
+
+# rate = 0
+# while rate <= 10:
+#    print("please increase rate till rate == 10: " , rate )
+#    rate = rate + 1
+
+# reqired = 0
+# no = 14
+# while no <= 14:
+#     no = no + 1
+#     print(no)+
+
+# def sumFun():
+#     print("hello sum")
+
+# sumFun()
+
+# def sumDo(a,b):
+#     return a+b
+
+# result = sumDo(35,52)
+# print(result)
+
+# def loopdo(num):
+#     for x in num:
+#         print(x)
+
+# num = [10,20,30,40,50,60,70]
+# fruits = ['apple', 'banana', 'mango', 'orange', 'grapes']
+# # result = loopdo(num)
+# result = loopdo(fruits)
+# print(result)
+
+
+
+
         
